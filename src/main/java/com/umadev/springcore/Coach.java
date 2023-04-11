791e54984c8aee40e34ca0c1f4878ec1c6be963a
uma-dev <22565959+uma-dev@users.noreply.github.com>
2023-04-11 15:37:14 -0600

    Added Dependency injection

---
+++ src/main/java/com/umadev/springcore/Coach.java
@@ -0,0 +1,6 @@
+package com.umadev.springcore;
+
+public interface Coach {
+    
+    String getDailyWorkout();
+}
---
+++ src/main/java/com/umadev/springcore/MyController.java
@@ -0,0 +1,22 @@
+package com.umadev.springcore;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MyController {
+    // Define a private field for the coach
+    private Coach myCoach; 
+
+    // Define the constructor for dependency injection
+    @Autowired
+    public MyController(Coach theCoach){
+        myCoach = theCoach;
+    }
+
+    @GetMapping("/dailyworkout")
+    public String getDailyWorkout(){
+        return myCoach.getDailyWorkout();
+    }
+}

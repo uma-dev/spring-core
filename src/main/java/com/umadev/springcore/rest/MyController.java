c774b7f432d3e7bf2669c1de8adce01fa8c488f5
uma-dev <22565959+uma-dev@users.noreply.github.com>
2023-05-01 23:57:03 -0600

    Removed redundant comment

--- src/main/java/com/umadev/springcore/rest/MyController.java
+++ src/main/java/com/umadev/springcore/rest/MyController.java
@@ -10,33 +10,36 @@ import com.umadev.springcore.common.Coach;
 @RestController
 public class MyController {
     // Define a private field for the coach
-    private Coach myCoach; 
+    private Coach myCoach;
 
     // Constructor injection RECOMMENDED
-    // Define the constructor for dependency injection 
     // @Qualifier also recommended above @Primary annotation on the beam
     @Autowired
-    public MyController( @Qualifier("swimCoach") Coach theCoach){
+    public MyController(@Qualifier("swimCoach") Coach theCoach) {
         System.out.println("In class: " + getClass().getSimpleName());
         myCoach = theCoach;
     }
 
     // Field injection - NOT RECOMMENDED
-    /* No need to add constructors or setters but makes code 
-       harder to unit test.
-     @Autowired
-     private Coach myCoach 
-    */
+    /*
+     * No need to add constructors or setters but makes code
+     * harder to unit test.
+     * 
+     * @Autowired
+     * private Coach myCoach
+     */
 
     // Setter injection
-    /*@Autowired
-    public void setCoach(Coach theCoach){ //setter name doesn't matter, autowired makes the magic
-        myCoach = theCoach;
-    }
-    */
+    /*
+     * @Autowired
+     * public void setCoach(Coach theCoach){ //setter name doesn't matter, autowired
+     * makes the magic
+     * myCoach = theCoach;
+     * }
+     */
 
     @GetMapping("/dailyworkout")
-    public String getDailyWorkout(){
+    public String getDailyWorkout() {
         return myCoach.getDailyWorkout();
     }
 
